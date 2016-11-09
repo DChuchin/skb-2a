14e4a594370d4e003f652ea16946d8b1f5da29c6
DmitryChuchin <dime10cents@gmail.com>
2016-11-09 10:55:23 +0600

    add cors

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-
+const cors = require('cors');
 const express = require('express');
 const app = express();
 
+app.use(cors());
 
 app.get('/', (req, res)=> {
-  let a = isNum(req.query.a) ? req.query.a : 0,
-      b = isNum(req.query.b) ? req.query.b : 0,
-      sum = parseInt(a) + parseInt(b);
+  const a =  req.query.a | 0,
+        b = req.query.b | 0,
+      sum = +a + +b;
   res.status(200).send(sum.toString());
 });
-
-function isNum(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
 
 app.listen(3000, ()=> {
   console.log('listening port 3000');
